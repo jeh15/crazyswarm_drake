12639fb7a919e0ad4bfe81c77a53902f1acee10d
jeh15 <jeh15@fsu.edu>
2022-10-10 14:11:15 -0400

    Added diplay setting for docker run file

--- run_container.sh
+++ run_container.sh
@@ -1,3 +1,15 @@
+# XAuthority Docker Path:
+XAUTH=/tmp/.docker.xauth
+
+# Give local user authority of X display: (Not safe)
+xhost +local:
+
 docker run --rm -it \
---volume="$PWD/scripts/:/scripts/" \
-jeh15/drone:latest
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    --gpus=all \
+    --volume="$PWD/scripts/:/scripts/" \
+    jeh15/drone:latest
