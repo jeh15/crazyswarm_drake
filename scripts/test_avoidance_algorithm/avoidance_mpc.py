a9145ba0755ab5a156b2a85c4c85b86e05b6d463
jeh15 <jeh15@fsu.edu>
2023-01-24 17:34:42 -0500

    TODO: Add Avoidance Algorithm.

--- scripts/test_avoidance_algorithm/avoidance_mpc.py
+++ scripts/test_avoidance_algorithm/avoidance_mpc.py
@@ -1,70 +1,23 @@
 import argparse
-import cProfile as profile
-from contextlib import contextmanager
-import pstats
-import subprocess
-import signal
-import os
-import timeit
-
 import numpy as np
 import ml_collections
-
 
 from pydrake.systems.analysis import Simulator
 from pydrake.systems.framework import DiagramBuilder
 
 # Custom LeafSystems:
-# import motion_planner_jax as motion_planner
-import motion_planner_jax_euler as motion_planner
+import motion_planner
 import reference_trajectory
 import trajectory_parser
 import crazyswarm_class
 
-@contextmanager
-def use_cprofile(output_file):
-    """
-    Use cprofile in specific context.
-    """
-    pr = profile.Profile()
-    pr.enable()
-    yield
-    pr.disable()
-    stats = pstats.Stats(pr)
-    stats.sort_stats("tottime", "cumtime")
-    stats.dump_stats(output_file)
-
-@contextmanager
-def use_py_spy(output_file, *, native=True, sudo=True):
-    """Use py-spy in specific context."""
-    args = [
-        "py-spy",
-        "record",
-        "-o", output_file,
-        "--pid", str(os.getpid()),
-    ]
-    if native:
-        # This will include C++ symbols as well, which will help determine if
-        # there which C++ functions may be slowing things down. However, you
-        # will need to dig or Ctrl+F to find out what Python code is going
-        # slow. Using Ctrl+F, searching for ".py:" should highlight Python
-        # code.
-        args += ["--native"]
-    if sudo:
-        args = ["sudo"] + args
-    p = subprocess.Popen(args)
-    try:
-        yield
-    finally:
-        p.send_signal(signal.SIGINT)
-        p.wait()
 
 # Convenient Data Class:
 def get_config() -> ml_collections.ConfigDict():
     config = ml_collections.ConfigDict()
     # Control Rates:
-    config.motion_planner_rate = 1.0 / 40.0
-    config.reference_trajectory_rate = 1.0 / 40.0
+    config.motion_planner_rate = 1.0 / 50.0
+    config.reference_trajectory_rate = 1.0 / 50.0
     config.crazyswarm_rate = 1.0 / 100.0
     # Model Parameters:
     config.nodes = 21                   # (Discretized Points)
@@ -75,16 +28,6 @@ def get_config() -> ml_collections.ConfigDict():
     return config
 
 def main(argv=None):
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--cprofile", type=str)
-    parser.add_argument("--py_spy", type=str)
-    parser.add_argument(
-        "--py_spy_no_native",
-        dest="py_spy_native",
-        action="store_false",
-    )
-    args = parser.parse_args()
-
     # Create Config Dict:
     params = get_config()
 
@@ -143,24 +86,28 @@ def main(argv=None):
     simulator.Initialize()
 
     # Simulate System:
-    benchmark_time = 5.0
-    wall_t_end = 1.0
+    FINAL_TIME = 20.0
+    dt = 1.0
+    next_time_step = dt
 
-    if args.py_spy is not None:
-        with use_py_spy(args.py_spy, native=args.py_spy_native):
-            wall_t_start = timeit.default_timer()
-            simulator.AdvanceTo(benchmark_time)
-            wall_t_end = timeit.default_timer() - wall_t_start
-    if args.cprofile is not None:
-        with use_cprofile(args.cprofile):
-            wall_t_start = timeit.default_timer()
-            simulator.AdvanceTo(benchmark_time)
-            wall_t_end = timeit.default_timer() - wall_t_start
-    
+    # w/ while-loop:
+    # while next_time_step <= FINAL_TIME:
+    #     print(f"Drake Real Time Rate: {simulator.get_actual_realtime_rate()}")
+    #     try:
+    #         simulator.AdvanceTo(next_time_step)
+    #         next_time_step += dt
+    #     except:
+    #         print(f"Exception Occurred...")
+    #         driver_crazyswarm.execute_landing_sequence()
+    #         break
+
+    # w/o while-loop:
+    simulator.AdvanceTo(FINAL_TIME)
+    print(f"Drake Real Time Rate: {simulator.get_actual_realtime_rate()}")
+             
+    # Land the Drone:
     driver_crazyswarm.execute_landing_sequence()
-    
-    realtime_factor = benchmark_time / wall_t_end     
-    print(f"realtime factor: {realtime_factor:.3f}")
+
 
 if __name__ == "__main__":
     main()
